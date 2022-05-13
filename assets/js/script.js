db525251012e9be64999ed45714e899cc4d41f93
brittanymy <brittany.young2001@gmail.com>
2022-05-12 21:25:53 -0400

    js comments added

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// HTML Element References
+let searchCity = document.getElementById ('search-city'),
+     searchBtn = document.getElementById ('search-btn'),
+     currentWeather = document.getElementById ('current-weather'),
+     currentCity = document.getElementById ('current-city'),
+     currentTemperature = document.getElementById ('current-temp'),
+     currentWind = document.getElementById ('current-wind'),
+     currentHumidity = document.getElementById ('current-humidity'),
+     uvIndex =  document.getElementById ('uv-index')
+     
+let city = '';
+let citySearch = [];
+
+// Searches Cities 
+
+// API Key Setup
+
+// Display current & future weather 
+
+// AJAX Call
+
+    // Query url
+
+// Returns UVIndex 
+
+// Display 5 Day Forecast 
+
+// Add passed city to search history
+
+// Display past search 
+
+// Render Function
+
+// Final Event Listeners
+
